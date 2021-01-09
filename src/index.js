22dabc288172bcdeb17acaacc8dca0e669d42162
alexsandrbarabash <sbarabas176@gmail.com>
2021-01-09 19:11:36 +0200

    created architecture project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-// const path = require("path");
-//
-// module.exports =
+const path = require("path");
+
+module.exports = {
+    context: path.join(__dirname, "src"),
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
